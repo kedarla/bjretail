fe93e178d545221e67dab62431fbc6bd320997e2
Saurabh Bongale <saurabh@letsalign.com>
2014-07-10 20:36:51 +0530

    Rename print attachment to printable_photo.

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,11 +1,11 @@
 class Option < ActiveRecord::Base
   belongs_to :part
   has_attached_file :photo, :styles => { :small => "150x150>" }
-  has_attached_file :print, :styles => { :small => "150x150>" }
+  has_attached_file :printable_photo, :styles => { :small => "150x150>" }
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
-  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
-  validates_attachment_presence :print
-  validates_attachment_size :print, :less_than => 5.megabytes
-  validates_attachment_content_type :print, :content_type => ['image/jpeg', 'image/png']
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
+  validates_attachment_presence :printable_photo
+  validates_attachment_size :printable_photo, :less_than => 5.megabytes
+  validates_attachment_content_type :printable_photo, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
 end
