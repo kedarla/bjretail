cbfa5ff44d1ba10ba066760186b2f97f49429be7
Saurabh Bongale <saurabh@letsalign.com>
2014-09-17 15:28:25 -0500

    added overlay for disabled options

--- app/assets/javascripts/garments.js
+++ app/assets/javascripts/garments.js
@@ -3,11 +3,14 @@ $(function() {
     $("input.c_disabled").each(function() {
       $(this).prop("disabled", false);
       $(this).removeClass("c_disabled");
+      $(this).closest(".part_option_wrapper").find(".option_overlay").remove();
     });
   	elem = $(this);
     disables = JSON.parse(elem.attr("data-option-disables"));
     for(i = 0; i < disables.length; i++) {
-      $("input[data-option-subid='option_subid_" + disables[i] + "']").prop("disabled", true).addClass("c_disabled");
+      elem_to_be_disabled = $("input[data-option-subid='option_subid_" + disables[i] + "']");
+      elem_to_be_disabled.prop("disabled", true).addClass("c_disabled");
+      elem_to_be_disabled.closest(".part_option_wrapper").prepend("<div class='option_overlay'/>");
     }
   	$("#selected_part_" + elem.attr("data-option-part")).find(".selected_part_option").html(elem.attr("data-option-name"));
   });
