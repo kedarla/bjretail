d24c1976e0f5e0bdb96e3b5971c82c7957d5bf72
Saurabh Bongale <saurabh@letsalign.com>
2014-09-17 12:00:29 -0500

    show child part

--- app/helpers/parts_helper.rb
+++ app/helpers/parts_helper.rb
@@ -1,2 +1,9 @@
 module PartsHelper
+  def options_for_part_child(part_child)
+  	options_str = ""
+  	part_child.options.each do |o|
+  	  options_str += "<option value='#{o.id}' data-option-subid='option_subid_#{o.id}' #{o.is_default? ? 'selected' : ''}>#{o.name}</option>"
+  	end
+  	options_str.html_safe
+  end
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -2,12 +2,12 @@ class Option < ActiveRecord::Base
   belongs_to :part
   has_attached_file :photo, :styles => { :small => "150x150>" }
   has_attached_file :printable_photo, :styles => { :small => "150x150>" }
-  validates_attachment_presence :photo
-  validates_attachment_size :photo, :less_than => 5.megabytes
-  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
-  validates_attachment_presence :printable_photo
-  validates_attachment_size :printable_photo, :less_than => 5.megabytes
-  validates_attachment_content_type :printable_photo, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
+  # validates_attachment_presence :photo
+  # validates_attachment_size :photo, :less_than => 5.megabytes
+  # validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
+  # validates_attachment_presence :printable_photo
+  # validates_attachment_size :printable_photo, :less_than => 5.megabytes
+  # validates_attachment_content_type :printable_photo, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
   default_scope order('created_at DESC')
 
   has_many :disables, :class_name => 'Disable', :foreign_key => 'option_id'
