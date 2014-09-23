03066ce963f6e09a2f6387e74d8cd14f68197ad3
Saurabh Bongale <saurabh@letsalign.com>
2014-09-23 09:23:19 -0500

    enable/disable related options bug fix

--- app/assets/javascripts/garments.js
+++ app/assets/javascripts/garments.js
@@ -1,11 +1,18 @@
 $(function() {
   $(".radio_option").on("change", function() {
-    $("input.c_disabled").each(function() {
-      $(this).prop("disabled", false);
-      $(this).removeClass("c_disabled");
-      $(this).closest(".part_option_wrapper").find(".option_overlay").remove();
-    });
   	elem = $(this);
+
+    grouped_inputs = $("input[data-option-group='" + elem.attr("data-option-group") + "']");
+    grouped_inputs.each(function() {
+      gi = $(this);
+      gi_disables = JSON.parse(gi.attr("data-option-disables"));
+      for(i = 0; i < gi_disables.length; i++) {
+        elem_to_enable = $("input[data-option-subid='option_subid_" + gi_disables[i] + "']");
+        elem_to_enable.prop("disabled", false).removeClass("c_disabled").closest(".part_option_wrapper").find(".option_overlay").remove();;
+      }
+    });
+
+    // Disable related options
     disables = JSON.parse(elem.attr("data-option-disables"));
     for(i = 0; i < disables.length; i++) {
       elem_to_be_disabled = $("input[data-option-subid='option_subid_" + disables[i] + "']");
