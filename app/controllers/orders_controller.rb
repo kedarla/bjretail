3408bf27553bd7c308d3ef28318a09411744ae59
Saurabh Bongale <saurabh@letsalign.com>
2014-11-03 06:26:37 -0600

    print sections on order show page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
   # GET /orders/1
   # GET /orders/1.json
   def show
+    @print_sections = PrintSection.all
     parts_arr = []
     options_arr = []
 
