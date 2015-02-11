dc5b360cef6a2afbf4af32a268e6b083bcd59396
Nirav Shah <nirav@Niravs-MacBook-Air.local>
2015-02-11 17:10:54 +0530

    fix to display dropdown

--- app/models/disable.rb
+++ app/models/disable.rb
@@ -1,5 +1,5 @@
 class Disable < ActiveRecord::Base
   belongs_to :option
-  belongs_to :disabler, :class_name => 'Option'
-  belongs_to :disabled, :class_name => 'Option'
+  #belongs_to :disabler, :class_name => 'Option', :foreign_key => 'disable_element_id',
+  belongs_to :disabled, :class_name => 'Option', :foreign_key => 'disable_element_id'
 end
--- app/models/enable.rb
+++ app/models/enable.rb
@@ -1,5 +1,7 @@
 class Enable < ActiveRecord::Base
   belongs_to :option
-  belongs_to :enabler, :class_name => 'Option'
-  belongs_to :enabled, :class_name => 'Option'
+  #belongs_to :enabler, :class_name => 'Option'
+  #belongs_to :enabled, :class_name => 'Option'
+  belongs_to :enabled, :class_name => 'Option', :foreign_key => 'enable_element_id'
+
 end
